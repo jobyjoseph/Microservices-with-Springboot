50b093f2fff4eaefa9cc8afd3205dd9e8921fc66
Joby Joseph <joby.joseph@litmus7.com>
2020-01-04 18:49:10 +0530

    Added path variable support

--- src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.jobyjoseph.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,4 +17,8 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World!");
     }
 
+    @GetMapping(path = "/hello-world/{name}")
+    public String helloWorldPath(@PathVariable String name) {
+        return "Hello " + name;
+    }
 }
