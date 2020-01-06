a283e6e27b6f4f87caecb9eb9ddc0a6078fdd2a6
Joby Joseph <joby.joseph@litmus7.com>
2020-01-06 15:34:57 +0530

    Implementing validations to REST services

--- src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/user/User.java
@@ -1,11 +1,17 @@
 package com.jobyjoseph.rest.webservices.restfulwebservices.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+
+    @Size(min=2, message = "Name should be minimum 2 characters")
     private String name;
+
+    @Past
     private Date birthDate;
 
     public User(Integer id, String name, Date birthDate) {
--- src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/jobyjoseph/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -28,9 +29,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) throws Exception {
-        if(user.getName() == null || user.getBirthDate() == null)
-            throw new Exception("Name and BirthDate required");
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
